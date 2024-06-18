420fd81cc6d2a118f510b9bc9b7050afe967d3f0
ShreyasGanesh911 <shreyas.ganesh911@gmail.com>
2024-06-18 23:15:50 +0530

    code cleanup

--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -1,57 +1,30 @@
-import React, { useState } from 'react'
-import Editor, { DiffEditor, useMonaco, loader } from '@monaco-editor/react';
-type Output = {
-  language: string,
-  version : string,
-  run : {
-    output : string
-  }
-}
+import { useEffect, useState } from 'react'
+import Editor from '@monaco-editor/react';
+import Output from '../components/Output';
+import EditorHead from '../components/EditorHead';
+import { ToastContainer } from 'react-toastify';
+
 export default function Home() {
   const [value,setValue] = useState<string[]>()
   const [code,setCode] = useState<string | undefined>()
-  const handleClick = async(e:React.MouseEvent<HTMLButtonElement, MouseEvent>)=>{
-    console.log(code)
-    const body = {
-      language : "javascript",
-      version : "18.15.0",
-      files :[
-        {
-          content :code
-        }
-      ]
-    }
-    try{
-      const response = await fetch("https://emkc.org/api/v2/piston/execute",{
-        method:"POST",
-        headers:{
-          "Content-Type": "application/json",
-          
-        },
-        body: JSON.stringify(body)
-      })
-    const data:Output = await response.json()
-    const repo = data.run.output.split('\n')
-    //setValue()
-    setValue(repo)
-    console.log(data.run.output.split('\n'))
-    console.log(data)
-    }catch(e){
-      console.log(e)
-    }
-  }
+  const [error,setError] = useState<boolean>(false)
+  
+  // useEffect(()=>{
+  //   window.onbeforeunload = (e)=>{
+  //       return ""
+  //   }
+  // })
   return (
     <>
-     <section className='page flex flex-col '>
-     <Editor height="90vh" theme='vs-dark'  defaultLanguage='javascript' width="50vw" language='python'  defaultValue="// some comment" value={code} onChange={(e)=>setCode(e)} />
-
-     <button onClick={handleClick}>click me</button>
-     <div className="w-full bg-pink-100 h-96">
-        {value?.map(e=><h1>{e}</h1>)}
-
-     </div>
+     <section className='page flex bg-black p-5'>
+        <Editor height="90vh" theme='vs-dark'  defaultLanguage='javascript' width="50vw" language='python'  defaultValue="// some comment" value={code} onChange={(e)=>setCode(e)} />
+        <div className='w-1/2 p-1'>
+          <EditorHead code={code} setError={setError} setValue={setValue}/>
+          <h2 className='text-white p-2 w-full border-b'>Output</h2>
+          <Output error={error} value={value}/>
+        </div>
     </section> 
+    <ToastContainer />
     </>
   )
 }
-// https://emkc.org/api/v2/piston
