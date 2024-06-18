9702abf12b6d92bb82038bf413b94657b97e167f
ShreyasGanesh911 <shreyas.ganesh911@gmail.com>
2024-06-18 21:09:27 +0530

    output component

---
+++ src/components/Output.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import type { RootState } from '../Redux/store'
+import { useSelector, useDispatch } from 'react-redux'
+import { languageSetter } from '../Redux/Slice/Language'
+type Props = {
+    error : boolean,
+    value : string[] | undefined
+}
+
+export default function Output({value,error}:Props) {
+    const language = useSelector((state: RootState) => state.language.value)
+    const dispatch = useDispatch()
+  return (
+    <>
+    <div className={` my-2 p-5 w-full bg-slate-800 border-2 text-white h-96 ${error ? 'border-red-500' :''}`}>
+        {value?.map(e=><h1>{e}</h1>)}
+     </div>
+    </>
+  )
+}
