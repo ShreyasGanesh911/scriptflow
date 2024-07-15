80f202e7d69b00ca38e00d8f572721e9cd41b762
ShreyasGanesh911 <shreyas.ganesh911@gmail.com>
2024-07-15 18:52:45 +0530

    added snippets

--- src/Constants/Snippets.ts
+++ src/Constants/Snippets.ts
@@ -1,1 +1,24 @@
-export const snippets:string[] = ["","console.log('Hello World')"]
+export const snippets={
+        java:
+`class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World!"); 
+        // Hello World!
+    }
+}
+        `,
+        javascript:`const greet = (a)=>{
+    console.log(a)
+}
+greet("Hello world")
+        `,
+        typescript:
+`const greet = (a:string)=>{
+    console.log(a)
+}
+greet("Hello world")
+        `
+}
+
+
+
