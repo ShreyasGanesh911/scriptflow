c8620940beb0324f326ba58eff1a7a7a2419a2ce
ShreyasGanesh911 <shreyas.ganesh911@gmail.com>
2024-07-15 18:52:59 +0530

    added routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,16 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./Pages/Home";
+import EditorPage from "./Pages/EditorPage";
 
 function App() {
   return (
       <>
-      <Home/>
+      <BrowserRouter>
+        <Routes>
+          <Route element={<Home/>} path="/"></Route>
+          <Route element={<EditorPage/>} path="/editor"></Route>
+        </Routes>
+      </BrowserRouter>
       </>
   );
 }
