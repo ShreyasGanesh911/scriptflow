6e60c852d2e91bb8e8b1a0f6a2ab00439d1ddc6e
ShreyasGanesh911 <shreyas.ganesh911@gmail.com>
2024-07-15 19:00:37 +0530

    Added new route

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./Pages/Home";
 import EditorPage from "./Pages/EditorPage";
+import PlayGround from "./Pages/Playground";
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route element={<Home/>} path="/"></Route>
+          <Route element={<PlayGround/>} path="/playground"></Route>
           <Route element={<EditorPage/>} path="/editor"></Route>
         </Routes>
       </BrowserRouter>
