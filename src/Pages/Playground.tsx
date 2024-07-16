2a05590fe1b7650d9a1f7497e8f425b3b6c16279
ShreyasGanesh911 <shreyas.ganesh911@gmail.com>
2024-07-16 23:53:39 +0530

    added save functionality

--- src/Pages/Playground.tsx
+++ src/Pages/Playground.tsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react'
 import Editor from '../components/Editor'
 import OutputConsole from '../components/OutputConsole'
 import { ToastContainer } from 'react-toastify'
+import axios from 'axios'
 
 export default function PlayGround() {
     const [html,setHtml] = useState('')
@@ -21,14 +22,26 @@ export default function PlayGround() {
       },500)
     },[html,css,js])
     
+    const handleSave = async(e:React.MouseEvent<HTMLButtonElement, MouseEvent>)=>{
+      const data = {
+        projectName:"Test1234", html,css,js,publicView:true
+      }
+      axios.post("http://localhost:4000/project/add",data,{withCredentials:true}).then((data)=>{
+          console.log(data)
+      }).catch((e)=>{
+        console.log((e))
+      })
+    }
   return (
     <>
-        <section className='page'>
-        <div className='flex w-full   items-start justify-evenly '>
+        <section className='page flex' >
+          
+        <div className=' w-1/4   items-start justify-evenly border' >
             <Editor language='html' font='fa-html5' setLanguage={setHtml} code={html}/> 
             <Editor language='css' setLanguage={setCss} font='fa-css3-alt' code={css}/>
             <Editor language='javascript' font='fa-square-js' setLanguage={setJs} code={js}/>
           </div> 
+          <button onClick={handleSave}>save</button>
       <OutputConsole srcDoc={srcDoc}/>    
       <ToastContainer/>
         </section> 
