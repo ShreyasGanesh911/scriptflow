ea5bbd06145d7e9011da7ee2ec0ab4632a875f8c
ShreyasGanesh911 <shreyas.ganesh911@gmail.com>
2024-07-16 23:53:30 +0530

    added landing page

--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -1,14 +1,37 @@
+import axios from "axios"
+import { useEffect, useState } from "react"
+import ProjectCard from "../components/ProjectCard"
+type Project = {
+  html :string,
+  css :string,
+  js :string,
+  projectName: string,
+  publicView : boolean 
+  user : {_id: string, name: string, email: string}
+  _id : string
+}
+type Data = {
+    success:boolean,
+    result:Project[]
+}
 export default function Home() {
- 
-  
-  // useEffect(()=>{
-  //   window.onbeforeunload = (e)=>{
-  //       return ""
-  //   }
-  // })
+  const [projects,setProjects] = useState<Project[]>()
+  useEffect(()=>{
+    axios.get("http://localhost:4000/project/allprojects", { withCredentials: true })
+    .then(({data,status})=>{
+      setProjects(data.result)
+      console.log(data,status)})
+    .catch((err)=>console.log(err))
+  },[])
   return (
     <>
-     
+     <section className="page flex flex-col items-center ">
+
+        <h1 className="w-3/4 xl:text-3xl lg:text-2xl p-3">Popular Playgrounds</h1>
+        <div className="w-3/4 grid 2xl:grid-cols-4 lg:grid-cols-3 border">
+        {projects?.map(e=><ProjectCard css={e.css} html={e.html} js={e.js} title={e.projectName} key={e._id}/>)}
+        </div>
+     </section>
     </>
   )
 }
