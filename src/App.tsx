35ba76bd19f811b3cb4bf2ba87deeabfc18d61fa
ShreyasGanesh911 <shreyas.ganesh911@gmail.com>
2024-07-16 23:52:25 +0530

    added new routes

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,22 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./Pages/Home";
 import EditorPage from "./Pages/EditorPage";
 import PlayGround from "./Pages/Playground";
+import Navbar from "./Global/Navbar";
+import Snippet from "./Pages/Snippet";
+import Login from "./Pages/Login";
 
 function App() {
   return (
       <>
       <BrowserRouter>
+      <Navbar/>
         <Routes>
+
           <Route element={<Home/>} path="/"></Route>
           <Route element={<PlayGround/>} path="/playground"></Route>
           <Route element={<EditorPage/>} path="/editor"></Route>
+          <Route element={<Snippet/>} path="/snippet/:id"></Route>
+          <Route element={<Login/>} path="/login"></Route>
         </Routes>
       </BrowserRouter>
       </>
