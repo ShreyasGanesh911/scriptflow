064ea93c4dd1bc784b31855af0881f8800c7ec33
ShreyasGanesh911 <shreyas.ganesh911@gmail.com>
2024-07-21 12:27:17 +0530

    Added env

--- Server/src/db/index.js
+++ Server/src/db/index.js
@@ -1,8 +1,8 @@
 import mongoose from 'mongoose'
-
+import 'dotenv/config'
 const connectMongoDb = async()=>{
   try{
-      await mongoose.connect("mongodb://localhost:27017/scriptflow")
+      await mongoose.connect(process.env.MONGO_URI)
       console.log("connected to data base")
 
   }catch(e){
